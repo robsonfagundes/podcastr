50eb80193ae5579169020ed4ca7b86b30a7a3e40
Robson Fagundes <robsonfagundes@gmail.com>
2021-04-28 21:47:11 -0300

    Add Header Component

--- src/pages/document.tsx
+++ src/pages/document.tsx
@@ -5,12 +5,13 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com">
-          <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet"> 
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" /> 
 				</Head>
-          <body>
-            <NextScript />
-          </body>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
 			</Html>
 		);
   }
