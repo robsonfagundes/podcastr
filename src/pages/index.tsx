7ef080b0625a6f51bd50c99ab1f98f63f937f238
Robson Fagundes <robsonfagundes@gmail.com>
2021-04-29 17:36:26 -0300

    Add object episode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
 
 import { GetStaticProps } from 'next';
 import { api } from '../services/api';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+
 
 type Episode = {
   id: string;
-  title: string;
-  members: string; 
+  title: string,
+  thumbnail: string
+  description: string,
+  members: string,
+  duration: number,
+  durationAtString: string,
+  url: string,
+  publishedAt: string,
 }
 
 type HomeProps = {
@@ -13,33 +23,54 @@ type HomeProps = {
 }
 
 export default function Home(props: HomeProps) {
-
-  console.log(props.episodes)
+  // console.log(props.episodes)
 
   return (
     <div>
       <h1>Index</h1>
 
-      {/* show api result */}
       <p>{JSON.stringify(props.episodes)}</p>
 
     </div>
   );
 
-
-  /**
-   * Consumindo uma API com next
-   * SPA - Single Page App
-   */
-
-  // useEffect(() => {
-  //   fetch('http://localhost:3333/episodes')
-  //     .then(response => response.json())
-  //     .then(data => console.log(data))
-  // }, []);
-
 }
 
+/**
+ * Consumindo uma API com next
+ * SSG - Static Side Generation 
+ */
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'publibshed_at',
+      _order: 'desc'
+    }
+  })
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      numbers: Number(episode.numbers),
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      duration: Number(episode.file.duation),
+      description: episode.description,
+      descriptionAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      urt: episode.file.url,
+    }
+  });
+
+  return {
+    props: {
+      episodes
+    },
+    revalidate: 60 * 60 * 8,
+  };
+
+}
 
 /**
  * Consumindo uma API com next
@@ -56,24 +87,14 @@ export default function Home(props: HomeProps) {
 //   }
 // }
 
-
 /**
  * Consumindo uma API com next
- * SSG - Static Side Generation 
+ * SPA - Single Page App
  */
-export const getStaticProps: GetStaticProps = async () => {
-  const {data} = await api.get('episodes', {
-    params: {
-      _limit: 12,
-      _sort: 'publibshed_at',
-      _order: 'desc'
-    }
-  })
 
-  return {
-    props: {
-      episodes: data,
-    },
-    revalidate: 60 * 60 * 8,
-  }
-}
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/episodes')
+  //     .then(response => response.json())
+  //     .then(data => console.log(data))
+  // }, []);
+
