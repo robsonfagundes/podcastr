8caa0bb1f6f2db8759af10e8ac1ce9a0e1a66556
Robson Fagundes <robsonfagundes@gmail.com>
2021-04-29 00:02:10 -0300

    Consuming an API with SPA, SSR, SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,55 @@
 
-export default function Home() {
+export default function Home(props) {
+
+  console.log(props.episodes)
+
+  /**
+   * Consumindo uma API com next
+   * SPA - Single Page App
+   */
+
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/episodes')
+  //     .then(response => response.json())
+  //     .then(data => console.log(data))
+  // }, []);
+
   return (
-    <h1>Index</h1>
+   <div>
+      <h1>Index</h1>
+    <p>{JSON.stringify(props.episodes)}</p>
+   </div>
   )
+}
+
+/**
+ * Consumindo uma API com next
+ * SSR - Server Side Render
+ */
+// export  async function getServerSideProps() {
+//   const response = await fetch('http://localhost:3333/episodes')
+//   const data = await response.json();
+ 
+//   return {
+//     props: {
+//       episodes: data,
+//     }
+//   }
+// }
+
+
+/**
+ * Consumindo uma API com next
+ * SSG - Static Side Generation 
+ */
+ export  async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json();
+ 
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
